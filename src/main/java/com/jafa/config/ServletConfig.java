966d382526c03a6074341fd05d0ecfaad864ddb0
dlaguswlsdla <dlaguswlsdla@naver.com>
2022-05-26 11:49:39 +0900

    마이바티스 연동

--- src/main/java/com/jafa/config/ServletConfig.java
+++ src/main/java/com/jafa/config/ServletConfig.java
@@ -13,7 +13,7 @@ public class ServletConfig implements WebMvcConfigurer{
 	@Override
 	public void configureViewResolvers(ViewResolverRegistry registry) {
 		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
-		viewResolver.setPrefix("/WEB-INF/views");
+		viewResolver.setPrefix("/WEB-INF/views/");
 		viewResolver.setSuffix(".jsp");
 		registry.viewResolver(viewResolver);
 	}
